e34e1e7b92aea788754849444502d60577537160
Rajshekar Eashwarappa <reashw@codeaurora.org>
2020-08-04 19:34:36 +0530

    Msm8998: WLAN enable
    Change-Id: I9cffa3c3fc24667df442d56724df69b0c1de8ad6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,7 @@ endif
 BOARD_VENDOR_KERNEL_MODULES := \
     $(KERNEL_MODULES_OUT)/wil6210.ko \
     $(KERNEL_MODULES_OUT)/msm_11ad_proxy.ko \
-    #$(KERNEL_MODULES_OUT)/qca_cld3_wlan.ko
+    $(KERNEL_MODULES_OUT)/qca_cld3_wlan.ko
 
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
