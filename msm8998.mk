29d0f178f38275c585de3dee0b4d3dd51f9f110c
Paras Nagda <pnagda@codeaurora.org>
2020-11-17 12:21:59 +0530

    msm8998: Copy default profiles to vendor/etc
    Copy default media_profiles.xml to vendor/etc
    
    Change-Id: Ia7ee7f3290cb729aba61bf061b76223ffcde5161

--- msm8998.mk
+++ msm8998.mk
@@ -40,7 +40,8 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8998/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     device/qcom/msm8998/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     device/qcom/msm8998/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    device/qcom/msm8998/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
+    device/qcom/msm8998/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
+    device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # Vendor property overrides
   PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank=0
